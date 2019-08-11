9ebbc1781f1632280396da3b360cf852dfaa5704
jianxia612 <swift_leaf54@126.com>
2019-08-11 23:33:30 +0800

    SpringBoot Admin

---
+++ springboot-admin/sc-eureka-server/src/main/java/com/whdcmap/study/ScEurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.whdcmap.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ScEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScEurekaServerApplication.class, args);
+    }
+
+}
+
